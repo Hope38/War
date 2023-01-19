70e509d57fb0a6a5d859307bafc187aab3c96a5d
Hope38 <122560567+Hope38@users.noreply.github.com>
2023-01-19 18:56:52 +0000

    New

--- The_Card_Deck.js
+++ The_Card_Deck.js
@@ -2,21 +2,23 @@ const SYMBOLS = ["♠", "♥", "♦","♣"]
 const VALUES = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
 
 
-class Deck {
-    constructor(cards) {
+export default class Deck {
+    constructor(cards = freshDeck()) {
         this.cards = cards
     }
 }
 
 class Card {
-    constructor(symbols, value){
+    constructor(symbols, value) {
         this.symbols = symbols
         this.value = value
     }
 }
 
 function freshDeck() {
-    return SYMBOLS.flatMap(suit --> {
-        return VALUES.map(value =>)
+    return SYMBOLS.flatMap(symbols --> {
+        return VALUES.map(value => {
+            return new Card(symbols, value)
+        })
     })
 }
