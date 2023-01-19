5be1a87aea53ae950cab64d9a35536791060c2f3
Hope38 <122560567+Hope38@users.noreply.github.com>
2023-01-19 19:06:51 +0000

    Next

--- The_Card_Deck.js
+++ The_Card_Deck.js
@@ -5,6 +5,11 @@ const VALUES = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
 export default class Deck {
     constructor(cards = freshDeck()) {
         this.cards = cards
+    
+}
+
+    shuffle() {
+        this.cards.sort((a, b) => Math.random() - .5)
     }
 }
 
@@ -16,7 +21,7 @@ class Card {
 }
 
 function freshDeck() {
-    return SYMBOLS.flatMap(symbols --> {
+    return SYMBOLS.flatMap(symbols => {
         return VALUES.map(value => {
             return new Card(symbols, value)
         })
