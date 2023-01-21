54988a4c54c256aadd30c59adf5b15724880d724
Hope38 <122560567+Hope38@users.noreply.github.com>
2023-01-21 10:42:09 -0500

    Update The_Card_Deck.js

--- The_Card_Deck.js
+++ The_Card_Deck.js
@@ -1,9 +1,11 @@
-const SYMBOLS = ["♠","♥","♦","♣"]
-const VALUES = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+//The full deck of cards
+const SYMBOLS = ["♠","♥","♦","♣"] //The symbols on the cards
+const VALUES = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"] //the numbers on the cards
 
 
 export default class Deck {
     constructor(cards = freshDeck()) {
+        // the cards in the deck
         this.cards = cards
     
 }
@@ -21,6 +23,7 @@ export default class Deck {
     }
 }
 
+//the individual cards
 class Card {
     constructor(symbols, value) {
         this.symbols = symbols
@@ -28,8 +31,11 @@ class Card {
     }
 }
 
+//A brand new deck of cards
 function freshDeck() {
+    //Loop through all the symbols
     return SYMBOLS.flatMap(symbols => {
+        //Loop through all the values
         return VALUES.map(value => {
             return new Card(symbols, value)
         })
