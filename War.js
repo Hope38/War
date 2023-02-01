846cbe4e5fb40b520b2ba2f4fb1a9c1bff4998a9
Hope38 <122560567+Hope38@users.noreply.github.com>
2023-02-01 18:52:30 +0000

    War.js fix

--- War.js
+++ War.js
@@ -1,9 +1,8 @@
 import Deck from "./The_Card_Deck.js"
 
-const computerCardSlot = document.querySelector('.computer-card-slot')
+const computerCardSlot = document.querySelector(".computer-card-slot")
 
 const deck = new Deck()
 deck.shuffle()
-console.log(deck.cards)
 
 computerCardSlot.appendChild(deck.cards[0].getHTML())
