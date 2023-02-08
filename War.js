65a3c5ec5b4866e65031419f6dfe375ad13f76e6
Hope38 <122560567+Hope38@users.noreply.github.com>
2023-02-08 18:27:26 +0000

    The game

--- War.js
+++ War.js
@@ -1,15 +1,37 @@
 import Deck from "./TheCardDeck.js"
 
+//will convert the Q,K,J into numbers
+const CARD_VALUE_MAP = {
+    "2":2,
+    "3":3,
+    "4":4,
+    "5":5,
+    "6":6,
+    "7":7,
+    "8":8,
+    "9":9,
+    "10":10,
+    "J":11,
+    "Q":12,
+    "K":13,
+    "A":14,
+}
+
 const computerCardSlot = document.querySelector('.computer-card-slot')
 const playerCardSlot = document.querySelector('.player-card-slot')
 const computerDeckElement = document.querySelector('.computer-deck')
 const playerDeckElement = document.querySelector('.player-deck')
 const text = document.querySelector('.text')
 
-let playerDeck, computerDeck, inRound
+let playerDeck, computerDeck, inRound, stop
 
 //if you click anywhere on the screen the function will run
 document.addEventListener('click', () => {
+    if(stop) {
+        startGame()
+        return
+    }
+
     if(inRound){
         //if the round is starting then clean it up
         cleanBeforeRound()
@@ -32,6 +54,7 @@ function startGame(){
     //the computers deck
     computerDeck = new Deck(deck.cards.slice(deckMidpoint, deck.NumberOfCards))
     inRound = false
+    stop = false
 
     //calls the function
     cleanBeforeRound()
@@ -59,6 +82,36 @@ function flipCards() {
     //renders the cards
     playerCardSlot.appendChild(playerCard.getHTML())
     computerCardSlot.appendChild(computerCard.getHTML())
+
+    updateDeckCount()
+
+    if (isRoundWinner(playerCard, computerCard)){
+        text.innerText = "Win"
+        //if the player wins then the card will return to them
+        playerDeck.push(playerCard)
+        playerDeck.push(computerCard)
+    } else if (isRoundWinner(computerCard, playerCard)){
+        text.innerText = "lose"
+        //if the player loses then the card is given to the computer
+        computerDeck.push(playerCard)
+        computerDeck.push(computerCard)
+    }else {
+        text.innerText = "Draw"
+        playerDeck.push(playerCard)
+        computerDeck.push(computerCard)
+    }
+
+    //if player runs out of cards then this will display
+    if (isGameOver(playerDeck)){
+        text.innerText = "You Lose!"
+        //will stop the game
+        stop = true
+    //if the computer runs out of cards then this will display
+    } else if (isGameOver(computerDeck)){
+        text.innerText = "You win!"
+        //will stop the game
+        stop = true
+    }
 }
 
 //updates the deck count
@@ -67,4 +120,11 @@ function updateDeckCount(){
     playerDeckElement.innerText = playerDeck.NumberOfCards
 }
 
+//Will determine who wins, will detemine which card is worth more
+function isRoundWinner(cardOne, cardTwo){
+    return CARD_VALUE_MAP[cardOne.value] > CARD_VALUE_MAP[cardTwo.value]
+}
 
+function isGameOver{
+
+}
